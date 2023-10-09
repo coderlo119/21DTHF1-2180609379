0e1369b573b50bcc78a4ccd3eab3dd2e70a7bdcc
coderlo119 <doantogiabao@gmail.com>
2023-10-09 10:32:27 +0700

    Nop Bai

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HS1 = new System.Windows.Forms.Label();
+            this.txtHS1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHS2 = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSub = new System.Windows.Forms.Button();
+            this.btnMul = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.txtAns = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // HS1
+            // 
+            this.HS1.AutoSize = true;
+            this.HS1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.HS1.Location = new System.Drawing.Point(168, 72);
+            this.HS1.Name = "HS1";
+            this.HS1.Size = new System.Drawing.Size(107, 31);
+            this.HS1.TabIndex = 0;
+            this.HS1.Text = "Hệ số 1";
+            this.HS1.Click += new System.EventHandler(this.HS1_Click);
+            // 
+            // txtHS1
+            // 
+            this.txtHS1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtHS1.Location = new System.Drawing.Point(313, 69);
+            this.txtHS1.Name = "txtHS1";
+            this.txtHS1.Size = new System.Drawing.Size(296, 38);
+            this.txtHS1.TabIndex = 1;
+            this.txtHS1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtHS1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHS1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(168, 161);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hệ số 2";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtHS2
+            // 
+            this.txtHS2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtHS2.Location = new System.Drawing.Point(313, 158);
+            this.txtHS2.Name = "txtHS2";
+            this.txtHS2.Size = new System.Drawing.Size(296, 38);
+            this.txtHS2.TabIndex = 2;
+            this.txtHS2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txtHS2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtHS2_KeyPress);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnAdd.Location = new System.Drawing.Point(175, 244);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(88, 49);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSub
+            // 
+            this.btnSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnSub.Location = new System.Drawing.Point(269, 244);
+            this.btnSub.Name = "btnSub";
+            this.btnSub.Size = new System.Drawing.Size(88, 49);
+            this.btnSub.TabIndex = 4;
+            this.btnSub.Text = "-";
+            this.btnSub.UseVisualStyleBackColor = true;
+            this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+            // 
+            // btnMul
+            // 
+            this.btnMul.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnMul.Location = new System.Drawing.Point(363, 244);
+            this.btnMul.Name = "btnMul";
+            this.btnMul.Size = new System.Drawing.Size(88, 49);
+            this.btnMul.TabIndex = 5;
+            this.btnMul.Text = "X";
+            this.btnMul.UseVisualStyleBackColor = true;
+            this.btnMul.Click += new System.EventHandler(this.btnMul_Click_1);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnDiv.Location = new System.Drawing.Point(457, 244);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(88, 49);
+            this.btnDiv.TabIndex = 6;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = true;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click_1);
+            // 
+            // txtAns
+            // 
+            this.txtAns.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.txtAns.Location = new System.Drawing.Point(321, 337);
+            this.txtAns.Name = "txtAns";
+            this.txtAns.ReadOnly = true;
+            this.txtAns.Size = new System.Drawing.Size(288, 38);
+            this.txtAns.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(205, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Answer";
+            // 
+            // btnBack
+            // 
+            this.btnBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnBack.Location = new System.Drawing.Point(619, 244);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(149, 49);
+            this.btnBack.TabIndex = 7;
+            this.btnBack.Text = "Trở lại";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAns);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMul);
+            this.Controls.Add(this.btnSub);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtHS2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtHS1);
+            this.Controls.Add(this.HS1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label HS1;
+        private System.Windows.Forms.TextBox txtHS1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHS2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.TextBox txtAns;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnBack;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+  
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HS1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            float HS1 = -1;
+            bool hs1Bool = float.TryParse(txtHS1.Text, out HS1);
+            if (!hs1Bool) 
+            {
+                    MessageBox.Show("Vui lòng nhập số ở ô Hệ số 1!");
+            }
+            float HS2 = -1;
+            bool hs2Bool = float.TryParse(txtHS2.Text, out HS2);
+            if (!hs2Bool)
+            {
+                MessageBox.Show("Vui lòng nhập số ở ô Hệ số 2!");
+            }
+            var ketQua = HS1 + HS2;
+            txtAns.Text = ketQua.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtHS1.Text = "";
+            txtHS2.Text = "";
+            txtAns.Text = "";
+            txtHS1.Focus();
+        }
+
+        private void btnSub_Click(object sender, EventArgs e)
+        {
+            float HS1 = -1;
+            bool hs1Bool = float.TryParse(txtHS1.Text, out HS1);
+            if (!hs1Bool)
+            {
+                MessageBox.Show("Vui lòng nhập số ở ô Hệ số 1!");
+            }
+            float HS2 = -1;
+            bool hs2Bool = float.TryParse(txtHS2.Text, out HS2);
+            if (!hs2Bool)
+            {
+                MessageBox.Show("Vui lòng nhập số ở ô Hệ số 2!");
+            }
+            var ketQua = HS1 - HS2;
+            txtAns.Text = ketQua.ToString();
+        }
+
+        private void btnMul_Click_1(object sender, EventArgs e)
+        {
+            float HS1 = -1;
+            bool hs1Bool = float.TryParse(txtHS1.Text, out HS1);
+            if (!hs1Bool)
+            {
+                MessageBox.Show("Vui lòng nhập số ở ô Hệ số 1!");
+            }
+            float HS2 = -1;
+            bool hs2Bool = float.TryParse(txtHS2.Text, out HS2);
+            if (!hs2Bool)
+            {
+                MessageBox.Show("Vui lòng nhập số ở ô Hệ số 2!");
+            }
+            var ketQua = HS1 * HS2;
+            txtAns.Text = ketQua.ToString();
+        }
+
+        private void btnDiv_Click_1(object sender, EventArgs e)
+        {
+            float HS1 = -1;
+            bool hs1Bool = float.TryParse(txtHS1.Text, out HS1);
+            if (!hs1Bool)
+            {
+                MessageBox.Show("Vui lòng nhập số ở ô Hệ số 1!");
+            }
+            float HS2 = -1;
+            bool hs2Bool = float.TryParse(txtHS2.Text, out HS2);
+            if (!hs2Bool)
+            {
+                MessageBox.Show("Vui lòng nhập số ở ô Hệ số 2!");
+            }
+            var ketQua = HS1 / HS2;
+            if (HS2 != 0)
+            {
+                txtAns.Text = ketQua.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Không thể chia một số cho 0!");
+            }
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtHS1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+                // Verify that the pressed key isn't CTRL or any non-numeric digit
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.') && (e.KeyChar != '-'))
+                {
+                    e.Handled = true;
+                }
+
+                // If you want, you can allow decimal (float) numbers
+                if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+                {
+                    e.Handled = true;
+                }
+        }
+
+        private void txtHS2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.') && (e.KeyChar != '-'))
+                {
+                    e.Handled = true;
+                }
+
+                // If you want, you can allow decimal (float) numbers
+                if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+                {
+                    e.Handled = true;
+                }
+        }
+    }
+}
